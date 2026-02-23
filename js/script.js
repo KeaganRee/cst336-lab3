1022a60535ce87b4383d27bdb7065da59faffa64
KeaganRee <kyan.lee109@gmail.com>
2026-02-22 20:03:55 -0800

    created site

---
+++ js/script.js
@@ -0,0 +1,105 @@
+//event listeners
+document.querySelector("button").addEventListener("click", gradeQuiz);
+
+shuffleQ1Choices();
+
+function shuffleQ1Choices(){
+    let q1Choices = ["Fire", "Water", "Earth", "Air"];
+    q1Choices = _.shuffle(q1Choices);
+    console.log(q1Choices);
+
+    for( let i of q1Choices){
+        let radioElement = document.createElement("input");
+        radioElement.type = "radio";
+        radioElement.name = "q1";
+        radioElement.value = i;
+
+        let labelElement = document.createElement("label");
+        labelElement.textContent = i
+
+        labelElement.prepend(radioElement);
+
+        document.querySelector("#q1ChoicesDiv").append(labelElement);
+
+        console.log(labelElement);
+    }
+}
+
+function gradeQuiz(){
+    let score = 0;
+    let answer1 = document.querySelector("input[name=q1]:checked").value;
+    if(answer1 == "Fire"){
+        document.querySelector("#image1C").style.display = "block";
+        document.querySelector("#image1X").style.display = "none";
+        score+=4;
+    }
+    else{
+        document.querySelector("#image1X").style.display = "block";
+        document.querySelector("#image1C").style.display = "none";
+    }
+
+    let answer2 = document.querySelector("#q2").value;
+
+    if(answer2 == "Toph" || answer2 == "toph"){
+        document.querySelector("#image2C").style.display = "block";
+        document.querySelector("#image2X").style.display = "none";
+        score+=4;
+    }
+    else{
+        document.querySelector("#image2X").style.display = "block";
+        document.querySelector("#image2C").style.display = "none";
+    }
+
+    let answer3 = document.querySelector("#q3").value;
+    console.log(answer3)
+
+    if(answer3 == "Sokka"){
+        document.querySelector("#image3C").style.display = "block";
+        document.querySelector("#image3X").style.display = "none";
+        score+=4;
+    }
+    else{
+        document.querySelector("#image3X").style.display = "block"
+        document.querySelector("#image3C").style.display = "none"
+    }
+
+    let answer4 = document.querySelector("#q4").value;
+    if(answer4 == 4){
+        document.querySelector("#image4C").style.display = "block"
+        document.querySelector("#image4X").style.display = "none"
+        score+=4;
+    }
+    else{
+        document.querySelector("#image4X").style.display = "block"
+        document.querySelector("#image4C").style.display = "none"
+    }
+
+    //checkbox taken from youtuber: OpenJavaScript tutorial
+    let check = document.querySelector(".checks");
+    let q5Answers = [];
+    let q5A = ["zuko", "iroh", "azula"];
+    document.querySelectorAll('[type="checkbox"]').forEach(item =>{
+        if(item.checked === true){
+            q5Answers.push(item.value);
+        }
+    })
+    if(JSON.stringify(q5Answers) === JSON.stringify(q5A)){
+        document.querySelector("#image5C").style.display = "block"
+        document.querySelector("#image5X").style.display = "none"
+        score+=4;
+    }
+    else{
+        document.querySelector("#image5X").style.display = "block"
+        document.querySelector("#image5C").style.display = "none"
+    }
+
+
+    if(score >= 16){
+        document.querySelector("#congrats").textContent = "Congratulations! You scored " + ((score/20)*100) + "%!"
+    }
+    else{
+        document.querySelector("#congrats").textContent = "Better luck next time! You scored " + ((score/20)*100) + "%!"
+    }
+
+}
+
